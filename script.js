6b64949e2e747e57ad73919971d568f935c2d9a3
nico83code <nico.man.codaisseur@gmail.com>
2020-06-15 22:16:46 +0200

    dag 2 opdr 1 intro JS

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hello Winc Academy");
+
+let firstName = "Nico";
+let lastName = "Man";
+console.log(`Mijn naam is ${firstName} ${lastName}`);
+
+let number1 = 6;
+let number2 = 2;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+//7.1 javascript ziet numbers als normale cijfers. je kan gewoon mee rekenen.
+
+let sum1 = "6";
+let sum2 = "2";
+console.log(sum1 + sum2);
+//7.2 met strings ziet javascipt als 2 lossen objecten. die plakt hij achter elkaar
+
+let age = 36;
+console.log(typeof age);
+
+let age1 = "36";
+console.log(typeof age1);
